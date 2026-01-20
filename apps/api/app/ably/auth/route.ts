ee73d5b15df378d924b683bc08120e7905f5897b
Angriff36 <unashamed366@gmail.com>
2026-01-20 04:36:19 -0800

    deps

--- apps/api/app/ably/auth/route.ts
+++ apps/api/app/ably/auth/route.ts
@@ -1,5 +1,5 @@
 import Ably from "ably";
-import { auth } from "@clerk/nextjs/server";
+import { auth } from "@repo/auth/server";
 import { env } from "@/env";
 import { NextResponse } from "next/server";
 
